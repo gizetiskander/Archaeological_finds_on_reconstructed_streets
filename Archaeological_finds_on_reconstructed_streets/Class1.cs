13cd309c3557d917c193008019267770d3cee52c
Гизетдинов Искандер <gizetiskander@gmail.com>
2021-06-07 21:14:29 +0300

    Добавьте файлы проекта.

---
+++ Archaeological_finds_on_reconstructed_streets/Class1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Archaeological_finds_on_reconstructed_streets
+{
+    public class Photo
+    {
+        public string Photos { get; set; }
+    }
+
+    public class GeoData
+    {
+        public List<double> coordinates { get; set; }
+        public string type { get; set; }
+    }
+
+    public class GeodataCenter
+    {
+        public string type { get; set; }
+        public string value { get; set; }
+    }
+
+    public class Root
+    {
+        public int ID { get; set; }
+        public string DescriptionOfFinding { get; set; }
+        public string DateOfDiscovery { get; set; }
+        public string DuringProgram { get; set; }
+        public List<Photo> Photo { get; set; }
+        public int global_id { get; set; }
+        public GeoData geoData { get; set; }
+        public GeodataCenter geodata_center { get; set; }
+    }
+}
---
+++ Archaeological_finds_on_reconstructed_streets/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using Newtonsoft.Json;
+
+namespace Archaeological_finds_on_reconstructed_streets
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {      
+            var url = $"https://apidata.mos.ru/v1/datasets/2541/features?appid=29e8fc889e30d97aed25e53bb6362aca&q=Moscow";
+
+            var request = WebRequest.Create(url);
+
+            var response = request.GetResponse();
+            var httpStatusCode = (response as HttpWebResponse).StatusCode;
+
+            if (httpStatusCode != HttpStatusCode.OK)
+            {
+                Console.WriteLine(httpStatusCode);
+                return;
+            }
+
+            using (var streamReader = new StreamReader(response.GetResponseStream()))
+            {
+                string result = streamReader.ReadToEnd();
+                Console.WriteLine(result);
+                var Geodatas = JsonConvert.DeserializeObject<Root>(result);
+                Console.WriteLine(Geodatas.geoData.coordinates);
+            }
+
+        }
+    }
+}
