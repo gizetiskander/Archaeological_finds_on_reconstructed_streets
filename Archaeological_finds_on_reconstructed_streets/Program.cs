4c128fe93287a7e8046f470da84a1f66ce028a6c
Гизетдинов Искандер <gizetiskander@gmail.com>
2021-06-14 20:06:51 +0300

    i fixed api_key

--- Archaeological_finds_on_reconstructed_streets/Program.cs
+++ Archaeological_finds_on_reconstructed_streets/Program.cs
@@ -11,7 +11,7 @@ namespace Archaeological_finds_on_reconstructed_streets
         {
             var ApiKey = "7ab05438c2866a22db1483c273a267ef";
             var City = "Moscow";
-            var url = $"https://apidata.mos.ru/v1/datasets/2541/features?appid={ApiKey}&q={City}&units=metric";
+            var url = $"https://apidata.mos.ru/v1/datasets/2541/features?api_key={ApiKey}&q={City}&units=metric";
 
             var request = WebRequest.Create(url);
 
